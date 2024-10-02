e8747e24ad2b88fb79dadcc62e7234c473320b53
Muhammad Abdullah <mabdullah@ogoul.com>
2024-10-02 16:20:30 +0500

    sum of two numbers completed

--- index.js
+++ index.js
@@ -2,5 +2,14 @@ const https = require("http");
 
 const server = https.createServer()
 
+
+// sum of two numbers
+
+const sumOfNumbers =(a,b)=>{
+    return( console.log(`sum of ${a} and ${b} is ${a + b}`))
+}
+ 
+sumOfNumbers(0 , 50)
+
 server.listen("5000" , ()=> console.log("server is running on port 3000"))
 
